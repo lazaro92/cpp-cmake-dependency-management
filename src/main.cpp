ded7233ad1aba805d7c80ebdfc74ad0517e6b450
albert <lazaroalbert92@gmail.com>
2024-05-22 21:27:49 +0200

    Added how to make a Debug / Release build

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,47 @@
 #include <SFML/Graphics.hpp>
+#include <iostream> 
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+int main() {
+  #if NDEBUG 
+    std::cout << "Debug mode is OFF" << std::endl;
+  #else 
+    std::cout << "Debug mode is ON" << std::endl;
+  #endif 
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+  sf::CircleShape shape(100.f);
+  
+  #if !NDEBUG
+    sf::Font font;
+    sf::Text text;
+
+    if (!font.loadFromFile("media/Sansation.ttf")) {
+      return -1;
+    }
+    text.setString("DEBUG MODE");
+    text.setFont(font);
+    text.setCharacterSize(24);
+    text.setFillColor(sf::Color::White);
+  #endif
+
+  shape.setFillColor(sf::Color::Green);
+
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
     }
 
-    return 0;
+    window.clear();
+    window.draw(shape);
+    #if !NDEBUG
+    window.draw(text);
+    #endif
+    window.display();
+  }
+
+  return 0;
 }
